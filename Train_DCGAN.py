21813398df961dae91e068a7e209fa3c160aced7
Nazmus-1225 <76961621+Nazmus-1225@users.noreply.github.com>
2024-03-03 11:15:43 +0600

    Update Train_DCGAN.py

--- Train_DCGAN.py
+++ Train_DCGAN.py
@@ -33,7 +33,7 @@ class DCGAN:
                 batch_filenames = np.random.random_integers(0, file_names.__len__()-1, BATCHSIZE)
                 
                 for j, filename in enumerate(batch_filenames):
-                    img = np.array(Image.open(file_path + file_names[filename]+".jpg"))
+                    img = np.array(Image.open(file_path + file_names[filename]))
                     h = img.shape[0]
                     w = img.shape[1]
                     batch[j, :, :, :] = transform.resize(img[(h // 2 - 70):(h // 2 + 70), (w // 2 - 70):(w // 2 + 70), :], [64, 64]) / 127.5 - 1.0
