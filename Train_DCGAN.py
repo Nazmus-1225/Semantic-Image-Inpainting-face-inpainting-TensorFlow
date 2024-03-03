b767ef3c8167790d2178e305ac31901d5bab5490
Nazmus-1225 <76961621+Nazmus-1225@users.noreply.github.com>
2024-03-03 11:47:27 +0600

    Update Train_DCGAN.py

--- Train_DCGAN.py
+++ Train_DCGAN.py
@@ -29,26 +29,36 @@ class DCGAN:
         print(len(file_names))
         saver = tf.train.Saver()
         batch = np.zeros([BATCHSIZE, IMG_H, IMG_W, IMG_C])
-        for i in range(8000):
-            for c in range(1):
-                batch_filenames = np.random.random_integers(0, file_names.__len__()-1, BATCHSIZE)
+        dataset_size = len(file_names)
+        num_epochs=50
+        num_steps = (dataset_size + BATCHSIZE - 1) // BATCHSIZE  # Calculate number of steps based on dataset size and batch size
+
+        for epoch in range(num_epochs):
+            for step in range(num_steps):
+                # Generate batch indices for this step
+                start_idx = step * BATCHSIZE
+                end_idx = min((step + 1) * BATCHSIZE, dataset_size)
+                batch_filenames = np.random.choice(file_names[start_idx:end_idx], BATCHSIZE, replace=True)
                 
                 for j, filename in enumerate(batch_filenames):
-                    img = np.array(Image.open(file_path + file_names[filename]))
+                    img = np.array(Image.open(file_path + filename))
                     h = img.shape[0]
                     w = img.shape[1]
                     batch[j, :, :, :] = transform.resize(img[(h // 2 - 70):(h // 2 + 70), (w // 2 - 70):(w // 2 + 70), :], [64, 64]) / 127.5 - 1.0
                 z = np.random.standard_normal([BATCHSIZE, Z_DIM])
                 self.sess.run(self.Opt_D, feed_dict={self.img: batch, self.z: z})
-            z = np.random.standard_normal([BATCHSIZE, Z_DIM])
-            self.sess.run(self.Opt_G, feed_dict={self.z: z})
-            if i % 10 == 0:
-                [D_loss, G_loss, fake_img] = self.sess.run([self.D_loss, self.G_loss, self.fake_img], feed_dict={self.img: batch, self.z: z})
-                print("Step: %d, D_loss: %f, G_loss: %f"%(i, D_loss, G_loss))
-                Image.fromarray(np.uint8((fake_img[0, :, :, :] + 1.0) * 127.5)).save("/kaggle/working/Semantic-Image-Inpainting-face-inpainting-TensorFlow/result/"+str(i)+".jpg")
-            if i % 100 == 0:
-                saver.save(self.sess, "/kaggle/working/Semantic-Image-Inpainting-face-inpainting-TensorFlow/save_para/dcgan.ckpt")
-gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+                
+                z = np.random.standard_normal([BATCHSIZE, Z_DIM])
+                self.sess.run(self.Opt_G, feed_dict={self.z: z})
+                
+                if epoch % 10 == 0:
+                    [D_loss, G_loss, fake_img] = self.sess.run([self.D_loss, self.G_loss, self.fake_img], feed_dict={self.img: batch, self.z: z})
+                    print("Epoch: %d, Step: %d, D_loss: %f, G_loss: %f"%(epoch, step, D_loss, G_loss))
+                    Image.fromarray(np.uint8((fake_img[0, :, :, :] + 1.0) * 127.5)).save("/kaggle/working/Semantic-Image-Inpainting-face-inpainting-TensorFlow/result/epoch_%d.jpg" % (epoch))
+                
+                if epoch % 10 == 0:
+                    saver.save(self.sess, "/kaggle/working/Semantic-Image-Inpainting-face-inpainting-TensorFlow/save_para/dcgan_epoch_%d.ckpt" % epoch)
+pu_devices = tf.config.experimental.list_physical_devices('GPU')
 for device in gpu_devices:
         tf.config.experimental.set_memory_growth(device, True)
 if __name__ == "__main__":
